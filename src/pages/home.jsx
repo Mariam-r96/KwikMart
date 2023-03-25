036e6580b346dde0a6c2ac79a47fbdaf20c37140
Mariyam-bs23 <mariyam.begom@bs-23.com>
2023-03-25 21:06:41 +0600

    display all products in home-page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React , {useState , useEffect} from 'react';
+import axios from 'axios';
 
 export default function Home(){
+    const [products , setProducts ] = useState([0]);
+  
+    useEffect(() => {
+      axios.get(`https://shodai.herokuapp.com/api/products`)
+      .then( response => {
+        setProducts(response.data);
+      })
+      .catch( error =>{
+        console.log(error);
+      });
+    }, []);
+
+    console.log("produtcs", products)
     return(
-        <h1 className='bg-red-200'>HOME</h1>
+        <div className='container mt-5'>
+            <h1 className='text-lg mb-8'>ALL PRODUCTS</h1>
+
+            <div className='grid grid-cols-5 items-start'>
+                {products && products.length > 0 && products.map((product , key) => {
+                    return(
+                        <div key={key} className='border border-gray-200 h-full'>
+                            <div className='h-36 w-full p-3'>
+                                <img 
+                                src={product.image}
+                                className='w-full h-full object-contain'/>
+                            </div>
+                            <div className='p-4 h-32'>
+                                <h5 className='text-gray-800 mt-3'>{product.title}</h5>
+                                <p className='mt-3 text-red-500'>$ {product.price}</p>
+                            </div>
+                            <div className='text-center p-6'>
+                                <button className='bg-sky-400 px-6 py-3 text-white rounded-xl'>ADD TO CART</button>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
     );
 }
